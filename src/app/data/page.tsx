944756cdb58ceac7aa7dfa34ab41df1a7348be64
ogane0112 <kai513272@gmail.com>
2024-10-13 15:29:15 +0900

    デザインを修正しました

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -2,7 +2,7 @@ import { CombinedEnvironmentalData, EnvironmentalDataChart } from '@/components/
 
 export default function EnvironmentalDashboard() {
     return (
-      <div className="space-y-8 mx-10">
+      <div className="space-y-8 ml-10">
         <CombinedEnvironmentalData />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           <EnvironmentalDataChart dataType="hum" title="湿度の推移" color="rgb(53, 162, 235)" yAxisLabel="湿度 (%)" />
--- src/app/humidity/page.tsx
+++ src/app/humidity/page.tsx
@@ -3,7 +3,7 @@ import { EnvironmentalDataChart } from '@/components/environmental-data';
 export default function HumidityPage() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-3xl font-bold mb-6">湿度データ</h1>
+      <h1 className="text-3xl  font-bold mb-6 text-center lg:text-left">湿度データ</h1>
       <EnvironmentalDataChart dataType="hum" title="湿度の推移" color="rgb(53, 162, 235)" yAxisLabel="湿度 (%)" />
     </div>
   );
